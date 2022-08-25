2bff32f2915f983d92e622f6374d4aef19136aff
vsinky <sinkykumariverma@gmail.com>
2022-08-25 19:41:36 +0530

    Added crossbrowser

---
+++ src/test/java/com/qa/demotestng/CrossBrowser.java
@@ -0,0 +1,64 @@
+package com.qa.demotestng;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CrossBrowser {
+	WebDriver driver;
+	@BeforeTest
+	@Parameters("browser")
+	public void setup(String browser) throws Exception{
+		//Check if parameter passed from TestNG is 'firefox'
+		if(browser.equalsIgnoreCase("firefox")){
+		//create firefox instance
+			System.setProperty("webdriver.gecko.driver","C:\\Users\\Admin\\Downloads\\geckodriver-v0.31.0-win64\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		//Check if parameter passed as 'chrome'
+		else if(browser.equalsIgnoreCase("chrome")){
+			//set path to chromedriver.exe
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			//create chrome instance
+			driver = new ChromeDriver();
+		}
+		//Check if parameter passed as 'Edge'
+				else if(browser.equalsIgnoreCase("Edge")){
+					//set path to Edge.exe
+					System.setProperty("webdriver.edge.driver","C:\\Users\\Admin\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+					//create Edge instance
+					driver = new EdgeDriver();
+				}
+		else{
+			//If no browser passed throw exception
+			throw new Exception("Browser is not correct");
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void testParameterWithXML() throws InterruptedException{
+		driver.get("https://www.facebook.com/");
+		//Find user name
+		WebElement userName = driver.findElement(By.name("email"));
+		//Fill user name
+		userName.sendKeys("sinkykumariverma@gmail.com");
+		//Find password
+		WebElement password = driver.findElement(By.id("pass"));
+		//Fill password
+		password.sendKeys("Sinky123");
+		WebElement login =driver.findElement(By.name("login"));
+			login.click();
+
+	}
+}
