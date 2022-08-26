ba0ecda00fe64f041f1218154c2e9d5e6c316c90
vsinky <sinkykumariverma@gmail.com>
2022-08-26 21:30:09 +0530

    Added testng program

---
+++ src/test/java/com/qa/demotestng/Asserts.java
@@ -0,0 +1,40 @@
+package com.qa.demotestng;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Asserts {
+	
+WebDriver driver;
+	
+	@Test (priority = 0)
+	public void CloseBrowser() {
+		//WebDriver.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+		driver= new ChromeDriver();
+		driver.close();
+		Reporter.log("Driver Closed After Testing");
+	}
+	
+	@Test (priority = -1)
+	public void OpenBrowser() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+	//	WebDriver.chromedriver().setup();
+		driver= new ChromeDriver();
+		Reporter.log("This test verifies the current selenium compatibility with TestNG by launching the chrome driver");
+	        Reporter.log("Launching Google Chrome Driver version 81 for this test"); 
+	        
+	        driver.get("https://www.demoqa.com");
+	        
+	        Reporter.log("The website used was DemoQA for this test", true);
+	        String expectedTitle = "Free QA Automation Tools For Everyone";
+	        String originalTitle = driver.getTitle();
+	        Assert.assertEquals(originalTitle, expectedTitle);
+  }
+}
